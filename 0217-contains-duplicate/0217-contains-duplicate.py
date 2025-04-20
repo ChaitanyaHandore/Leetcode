fcbe13eb87a721cf6f104b56e67eaf91e505f34f
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-04-20 10:10:45 +0100

    Time: 19 ms (24.9%), Space: 26 MB (16.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,11 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        from collections import Counter
-        c = Counter(nums)
-        for i,j in c.items():
-            if j>1:
-                return True
-        return False
+        return len(nums) != len(set(nums))
         
         
