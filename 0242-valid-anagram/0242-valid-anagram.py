f01e5ee1d413e412181c239461dfb8930ebd270f
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-04-20 12:21:30 +0100

    Time: 0 ms (100%), Space: 12.7 MB (67.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        from collections import Counter
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        for i in set(s):
+            if s.count(i) != t.count(i):
+                return False
+        return True
