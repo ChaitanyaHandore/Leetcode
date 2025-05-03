4ff6bfd52aa57da2071db7fb3ac7c99d75106fe2
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-05-03 21:01:45 +0100

    Time: 4 ms (13.11%), Space: 12.4 MB (83.61%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def kItemsWithMaximumSum(self, numOnes, numZeros, numNegOnes, k):
+        """
+        :type numOnes: int
+        :type numZeros: int
+        :type numNegOnes: int
+        :type k: int
+        :rtype: int
+        """
+        return min(k, numOnes) - max(0, k - numZeros - numOnes)
