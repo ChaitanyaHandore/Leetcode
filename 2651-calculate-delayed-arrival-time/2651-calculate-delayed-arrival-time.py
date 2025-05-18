c5b5c6513ccb8720aeb5a294ae3b0f3017c6e89a
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-05-18 19:20:28 +0100

    Time: 1 ms (10.64%), Space: 12.5 MB (51.06%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findDelayedArrivalTime(self, arrivalTime, delayedTime):
+        """
+        :type arrivalTime: int
+        :type delayedTime: int
+        :rtype: int
+        """
+        s = arrivalTime+delayedTime
+        if s == 24:
+            return 0
+        elif s>24:
+            return s-24
+        else:
+            return s
