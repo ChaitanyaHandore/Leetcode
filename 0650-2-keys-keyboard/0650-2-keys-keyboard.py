b6a0228ddfc01ef8d72ca37e98a550da4a461651
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-05-21 08:41:52 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minSteps(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 0
+        else:
+            return n
