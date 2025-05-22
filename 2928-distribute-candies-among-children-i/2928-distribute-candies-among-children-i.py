0e4ee89c19d249cf531f10a16bfefa8e54539153
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-05-22 21:13:38 +0100

    Time: 18 ms (79.31%), Space: 12.5 MB (55.17%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def distributeCandies(self, n, limit):
+        """
+        :type n: int
+        :type limit: int
+        :rtype: int
+        """
+        result = 0
+        for i in range(0,limit+1) :
+            for j in range(0,min(limit,n-i)+1) :
+                if n-i-j <= limit :
+                    result += 1
+        return result 
+        
