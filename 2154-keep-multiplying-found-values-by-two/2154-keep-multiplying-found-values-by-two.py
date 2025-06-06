8f3e9510af7b2d6da8e3029fa90bcd66d27702ed
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-06-06 12:01:59 +0100

    Time: 1 ms (33.33%), Space: 12.7 MB (11.9%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -1,15 +1,8 @@
 class Solution(object):
     def findFinalValue(self, nums, original):
-        """
-        :type nums: List[int]
-        :type original: int
-        :rtype: int
-        """
-        cmap = {num: True for num in nums}
-        while True:
-            if original in cmap:
-                original*=2
-            else:
-                break
+        nums = set (nums)
+        
+        while original in nums:
+            original *= 2
+
         return original
-                
