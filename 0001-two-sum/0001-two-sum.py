25edfed3f366bedd6e8c74116c1c91b959a017b5
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-08-05 22:57:44 +0100

    Time: 1782 ms (17.94%), Space: 18.5 MB (71.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        pair_idx = {}
-
-        for i, num in enumerate(nums):
-            if target - num in pair_idx:
-                return [i, pair_idx[target - num]]
-            pair_idx[num] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+        # Return an empty list if no solution is found
+        return []
