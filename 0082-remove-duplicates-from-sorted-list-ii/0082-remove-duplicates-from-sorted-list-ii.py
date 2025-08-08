e5eecf8a2910ff892fa2429d999ddeeb1f8f6516
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-08-08 20:58:42 +0100

    Time: 3 ms (28.08%), Space: 17.8 MB (42.11%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -1,34 +1,22 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        freq = {}
+        curr = head
+        while curr:
+            freq[curr.val] = freq.get(curr.val, 0) + 1
+            curr = curr.next
 
-from collections import Counter
-
-class Solution(object):
-    def deleteDuplicates(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        # Step 1: Count occurrences of each value
-        c_counts = Counter()
-        c_current = head
-        while c_current:
-            c_counts[c_current.val] += 1
-            c_current = c_current.next
-
-        # Step 2: Remove nodes with duplicate values
-        c_dummy = ListNode(0)  # Dummy node to handle edge cases
-        c_dummy.next = head
-        c_prev, c_current = c_dummy, head
-
-        while c_current:
-            if c_counts[c_current.val] > 1:  # If duplicate, skip the node
-                c_prev.next = c_current.next
-            else:
-                c_prev = c_current
-            c_current = c_current.next
-
-        return c_dummy.next
+        dummy = ListNode(0)
+        tail = dummy
+        curr = head
+        while curr:
+            if freq[curr.val] == 1:
+                tail.next = ListNode(curr.val)
+                tail = tail.next
+            curr = curr.next
+        return dummy.next
