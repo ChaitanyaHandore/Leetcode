25734459bd5eca3072cdafb2e83403545dc4f35d
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-08-17 18:24:45 +0100

    Time: 57 ms (68.28%), Space: 20.3 MB (20.78%) - LeetHub

--- 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.py
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.py
@@ -1,10 +1,12 @@
-class Solution(object):
-    def canReorderDoubled(self, A):
-        count = collections.Counter(A)
-        for x in sorted(A, key = abs):
-            if count[x] == 0: continue
-            if count[2*x] == 0: return False
-            count[x] -= 1
-            count[2*x] -= 1
-
-        return True
+class Solution:
+    def canReorderDoubled(self, arr: List[int]) -> bool:
+        if sum(arr)%3 != 0: return False
+        
+        expected = defaultdict(int)
+        for a in sorted(arr, key = abs):
+            if expected[a] > 0:
+                expected[a] -= 1
+            else:
+                expected[2*a] += 1
+        
+        return all(x == 0 for x in expected.values())
