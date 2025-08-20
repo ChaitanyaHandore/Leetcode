975816b2350ecceb90597245d8b9ed0284c87d28
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-08-20 21:08:00 +0100

    Time: 3 ms (49.42%), Space: 17.8 MB (39.15%) - LeetHub

--- 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.py
+++ 3545-minimum-deletions-for-at-most-k-distinct-characters/3545-minimum-deletions-for-at-most-k-distinct-characters.py
@@ -1,21 +1,7 @@
-from collections import Counter
+class Solution:
+    def minDeletion(self, s: str, k: int) -> int:
+        
+        ctr = defaultdict(int)
+        for ch in s: ctr[ch]+= 1
 
-class Solution(object):
-    def minDeletion(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        delet = 0
-        count = Counter(s)  # Automatically creates a dictionary with character counts
-
-        if len(count) < k:
-            return 0
-
-        freq = sorted(count.values())
-
-        while len(freq) > k:
-            delet += freq.pop(0)
-
-        return delet
+        return sum(nsmallest(len(ctr) - k, ctr.values()))
