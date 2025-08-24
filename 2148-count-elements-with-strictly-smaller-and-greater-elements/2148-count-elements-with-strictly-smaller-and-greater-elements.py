8c499db6655b2d942e8b03521b95afb980ae5049
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-08-24 21:35:50 +0100

    Time: 0 ms (100%), Space: 17.7 MB (88.62%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def countElements(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        mn = min(nums)
-        mx = max(nums)
-        return sum(1 for i in nums if mn < i < mx) 
+class Solution:
+    def countElements(self, nums: list[int]) -> int:
+        if len(set(nums)) == 1:
+            return 0
+        return len(nums) - nums.count(max(nums)) - nums.count(min(nums))
