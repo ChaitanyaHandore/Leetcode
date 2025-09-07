5dfb4d5306564340f134ba723901a64c7f9fc235
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-09-07 11:31:22 +0100

    Time: 0 ms (100%), Space: 19.5 MB (98.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,11 +1,24 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals = intervals+[newInterval]
-        intervals.sort(key=lambda x: x[0])
-        m =[]
-        for interval in intervals:
-            if not m or m[-1][1]<interval[0]:
-                m.append(interval)
-            else:
-                m[-1][1]=max(m[-1][1],interval[1])
-        return m
+        res = []
+        i = 0
+        n = len(intervals)
+
+        # 1) Left part (no overlap)
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        # 2) Merge part (overlap exists)
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+
+        # 3) Right part (no overlap)
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
