13b53c7e5e39163e3b9b30e920ee6a2aff6f2c72
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-09-13 21:00:28 +0100

    Time: 46 ms (71.36%), Space: 20 MB (29.33%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,20 +6,13 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        st = set()
-
-        while head is not None:
-
-            # if this node is already present
-            # in hashmap it means there is a cycle
-            if head in st:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
-
-            # if we are seeing the node for
-            # the first time, insert it in hash
-            st.add(head)
-
-            head = head.next
-
         return False
-            
+# TC: O(n)
+# SC: O(1)
