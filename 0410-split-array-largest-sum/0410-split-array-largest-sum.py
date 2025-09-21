7df7286bfd8625367a77329c03b8703590980ff4
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-09-21 12:27:33 +0100

    Time: 1 ms (77.16%), Space: 18 MB (41.74%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -2,28 +2,23 @@ from typing import List
 
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        # Greedy: can we split into <= k subarrays if no subarray sum exceeds `largest`?
-        def cansplit(largest: int) -> bool:
+        l = max(nums)
+        r = sum(nums)
+    
+        def split(mid):
             subarray = 1
             currsum = 0
-            for n in nums:
-                # we know largest >= max(nums), so no single n will break it
-                if currsum + n <= largest:
-                    currsum += n
-                else:
-                    subarray += 1
-                    currsum = n
-                    if subarray > k:   # early exit
-                        return False
-            return True  # used <= k subarrays
+            for i in nums:
+                currsum+=i
+                if currsum>mid:
+                    subarray+=1
+                    currsum = i
+            return subarray<=k
 
-        l, r = max(nums), sum(nums)
-        res = r
-        while l <= r:
-            mid = (l + r) // 2
-            if cansplit(mid):
-                res = mid
-                r = mid - 1
+        while l<r:
+            m = (l+r)//2
+            if split(m):
+                r = m
             else:
-                l = mid + 1
-        return res
+                l = m+1
+        return l
