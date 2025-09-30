5205e1af5f80e0e7799696bc63a8e7e3cd2f6a82
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-09-30 10:24:10 +0100

    Time: 119 ms (35.82%), Space: 26.9 MB (32.79%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,10 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        min_price = float('inf')
-        max_profit = 0
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')   # keep track of lowest price so far
+        max_profit = 0             # result
 
         for price in prices:
-            if price < min_price:
-                min_price = price  # Update minimum price
-            else:
-                max_profit = max(max_profit, price - min_price)  # Calculate profit
+            min_price = min(min_price, price)           # update min price
+            max_profit = max(max_profit, price - min_price)  # profit if sold today
 
         return max_profit
