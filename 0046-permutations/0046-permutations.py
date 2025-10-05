6167047521525bd665a0c52b1f064fc816e43431
Chaitanya Handore <125127771+ChaitanyaHandore@users.noreply.github.com>
2025-10-05 20:13:59 +0100

    Time: 3 ms (31.75%), Space: 18 MB (59.36%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,7 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        from itertools import permutations
-        # Generate all permutations
-        result = list(permutations(nums))
+        cres = []
+        cpath = []
+        cused = [False] * len(nums)
 
-        return result
+        def backtrack():
+            if len(cpath) == len(nums):
+                cres.append(cpath[:])
+                return
+
+            for ci in range(len(nums)):
+                if not cused[ci]:
+                    cpath.append(nums[ci])
+                    cused[ci] = True
+                    backtrack()
+                    cpath.pop()
+                    cused[ci] = False
+
+        backtrack()
+        return cres
